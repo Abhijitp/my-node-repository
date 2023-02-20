1079677d8e62183eaddbd87b5b035bb8d8c4598b
Abhijit Pattanaik <pattanaik.abhijit@gmail.com>
2023-02-20 21:09:38 +0530

    Trying to deploy

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
-
-
 import express  from 'express';
 import mongoose from 'mongoose';
 /*const path = require('path');
